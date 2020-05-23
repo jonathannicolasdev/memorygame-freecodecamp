f8d5ed77d416b830a45eea1b7184407d2cdb9bd0
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-05-22 19:48:16 -0700

    Differentiate first and second card when clicked

--- scripts.js
+++ scripts.js
@@ -3,12 +3,30 @@
 
 const cards = document.querySelectorAll(".memory-card");
 
-console.log(cards);
+// When a player clicks a card we must know if its the first or second card that is
+// being clicked
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-  console.log("hello");
-  console.log(this);
   this.classList.toggle("flip");
+  // console.log("hello");
+  // console.log(this);
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+    console.log({ firstCard, secondCard });
+    // now that we have both cards that we clicked we need to check if these two cards match
+    // we can add a data attribute to our element
+    console.log(firstCard.dataset.pokemon);
+    console.log(secondCard.dataset.pokemon);
+  }
+
+  // console.log({ hasFlippedCard, firstCard });
 }
 
 // Loop through that list of cards and to each one of the cards we will attach
